cd2247a59ded9656709a82dc9eae99614fc8a1da
Your Name <anoor@witweel.com>
2017-10-24 12:19:13 -0400

    navFixed

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React,{ Component }  from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
+// import Background from '../../img/websites-modern.jpg';
 
+// var sectionStyle = {
+//   width: "100%",
+//   height: "400px",
+//   backgroundImage: "url(" + { Background } + ")"
+// };
+
+// class Home extends Component {
+//   render() {
+//     return (
+//       <section style={ sectionStyle }>
+//       <img />
+//       <h2>Welcome to ACME Inc. Resume Database</h2>
+//       </section>
+//     );
+//   }
+//}
 const Home = () =>
   <Container fluid>
     <Row>
       <Col size="md-12">
-        <Jumbotron>
+        
           <h2>Welcome to ACME Inc. Resume Database</h2>
-          
-        </Jumbotron>
+            <section className="picture"></section> 
+        
       </Col>
     </Row>
   </Container>;
